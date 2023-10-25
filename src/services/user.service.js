ce762023522237aa2fa2dd1e2547eac898b13e6d
AFESAID <123980177+AFESAID@users.noreply.github.com>
2023-10-25 17:30:43 -0500

    build: :fire: Creando los modelos Area y Agenda

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -13,6 +13,25 @@ class UserService {
         const user = await this.model.create({ name, email, password, status })
         return user
     }
+
+    async findOne(id) {
+        const user = await this.model.findByPk(id)
+        return user
+    }
+
+    async update(id, values) {
+        const user = await this.findOne(id)
+        if (!user) return null
+        const updateUser = await user.update(values)
+        return updateUser
+    }
+
+    async delete(id) {
+        const user = await this.findOne(id)
+        if (!user) return null
+        await user.destroy()
+        return user.id
+    }
 }
 
 module.exports = UserService
